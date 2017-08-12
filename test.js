75dcfff936500ee75a0bcbd325a717d8d491bd75
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import splat from "./source"
+import startsWith from "./source"
 
 test(({ok, end}) => {
   ok(startsWith("I have")("I have no period"))
